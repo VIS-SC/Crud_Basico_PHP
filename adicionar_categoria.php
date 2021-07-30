0fef3aa640d5d96a7ae8bd7a2a5052e850eed933
VIS-SC <76501162+VIS-SC@users.noreply.github.com>
2021-07-29 23:13:30 -0300

    Criando formulario de categorias

--- adicionar_categoria.php
+++ adicionar_categoria.php
@@ -1,43 +1,43 @@
 <!DOCTYPE html>
-<html lang="pt-BR">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Menu</title>
-
+    <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <style type="text/css">
+
+        #botao{
+            background-color: #A2122F;
+            color: white;
+        }
+
+    </style>
 
 </head>
 <body>
     
-    <div class="container" style="margin-top: 100px;">
+    <div class="container" style="margin-top: 40px; width: 500px;">
 
-            <div class="row">
-        <div class="col-sm-6">
-            <div class="card">
-            <div class="card-body">
-                <h5 class="card-title">Adicionar Produto</h5>
-                <p class="card-text">Opcão para adicionar produto em nosso estoque.</p>
-                <a href="adicionar_produto.php" class="btn btn-primary">Cadastrar Produto</a>
+    <div style="text-align: right;">
+            <a href="index.php" role="button" class="btn btn-sm btn-primary">Voltar</a>
+    </div>
+
+        <h4>Cadastro de Categoria</h4>
+        <form action="_inserir_categoria.php" method="POST">
+            <label>Categoria</label>
+            <div class="form-group">
+                <input type="text" name="categoria" class="form-control" placeholder="Digite o nome da categoria">
             </div>
+            <div style="text-align: right; padding-top: 20px;" >
+              <button type="submit" id="botao" class=" btn btn-sm">Cadastrar</button>
             </div>
-        </div>
-        
-        <div class="col-sm-6">
-            <div class="card">
-            <div class="card-body">
-                <h5 class="card-title">Lista de Produto</h5>
-                <p class="card-text">Visualizar, editar e excluir os produtos.</p>
-                <a href="listar_produtos.php" class="btn btn-primary">Produtos</a>
-            </div>
-            </div>
-        </div>
+        </form>
+    </div>
+
     
-    </div>
-
-    </div>
-
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
